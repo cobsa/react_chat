bc446cb72990fd808263d3e3467d0074be19271e
Jaakko Koponen <cobsa89@gmail.com>
2017-12-07 14:41:16 +0200

    Added prettier and enzyme

--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 var app = document.getElementById('app')
-ReactDom.render( <h1>React is working</h1>, app);
+ReactDom.render(<h1>React is working</h1>, app)
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,14 @@
 var path = require('path')
 module.exports = {
-    entry: {
-        app: ["./src/main.js"]
-    },
-    output: {
-        path: path.resolve(__dirname, "build"),
-        publicPath: "/assets/",
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
-            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
-        ]
-    }  
-};
+  entry: {
+    app: ['./src/main.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/assets/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }]
+  }
+}
